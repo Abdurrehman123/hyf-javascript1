322bd2418a6e3726f56f488d11e126d316bc3cfe
Abdurrehman123 <35459026+Abdurrehman123@users.noreply.github.com>
2018-05-25 18:12:07 +0300

    JS week1

---
+++ week1/JS1_HW/home work.js
@@ -0,0 +1,91 @@
+// //Q Hellow word
+// console.log("hello world");
+// console.log("salam donya");
+// console.log("merheba allam");
+// // 1
+// console.log(i am awesom);
+// // 3
+// let x;
+// console.log('the value of variable x : ' + x);
+// console.log(x);
+// x = 10;
+// console.log('the value of variable x : ' + x);
+// console.log(x);
+// // 4
+// let y = 'text';
+// console.log('the value of string y: ' + y);
+// console.log(y);
+// y = 'new text';
+// console.log('the value of string y: ' + y);
+// console.log(y);
+// // 5
+// let z = 7.25;
+// console.log(z);
+// let a = Math.floor(z);
+// console.log(a);
+// let max = Math.max(z, a);
+// console.log(max);
+// // 6
+// let myNewArray = [];
+// console.log('the value of array will be empty array');
+// console.log(myNewArray);
+// let myFavoritAnimals = ['Lion', 'deer', 'Snake'];
+// console.log(myFavoritAnimals);
+// myFavoritAnimals.push('baby pig');
+// console.log(myFavoritAnimals);
+// // 7
+// let myString = 'this is a test';
+// console.log(myString);
+// console.log(myString.length);
+// // 8
+// let myNum = 3;
+// let myStr = '3';
+// let myArray = ['One', 'Two', 'Three'];
+// let myBoolean = true;
+// console.log('The value of variable myNum is: ' + myNum);
+// console.log('The value of variable MyStr is: ' + myStr);
+// console.log('The value of variable myArray is: ' + myArray);
+// console.log('The value of variable myBoolean is: ' + myBoolean);
+// console.log('The type of variable myNum is: Number');
+// console.log(typeof myNum);
+// console.log('The type of my variable myStr is: String');
+// console.log(typeof myString);
+// console.log('The type of my variable myArray is: Object');
+// console.log(typeof myArray);
+// console.log('The type of my variable myBoolean is: Boolean');
+// console.log(typeof myBoolean);
+// if (myNum === myStr) {
+//   console.log(myNum + ' and ' + myStr + ' have same type');
+// } else {
+//   console.log(myNum + ' and ' + myStr + ' have different types');
+// }
+// if (myStr === myArray) {
+//   console.log(myStr + ' and ' + myArray + ' have same type');
+// } else {
+//   console.log(myStr + ' and ' + myArray + ' have different types');
+// }
+// if (myArray === myBoolean) {
+//   console.log(myArray + ' and ' + myBoolean + ' have same type');
+// } else {
+//   console.log(myArray + ' and ' + myBoolean + ' have different types');
+// }
+// // 9
+// let n = 7;
+// n = n % 3;
+// console.log(n);
+// let f = 8;
+// f = f % 3;
+// console.log(f);
+// let k = 29;
+// k = k % 5;
+// console.log(k);
+// // 10
+// let DiffArray = [3, false, 'text', 1, 'car', true];
+// console.log(DiffArray);
+// let Num1 = 6/0;
+// let Num2 = 10/0;
+// if (Num1 === Num2) {
+//   console.log('I can compare infinities');
+// } else {
+//   console.log('I can\'t compare infinities');
+// }
